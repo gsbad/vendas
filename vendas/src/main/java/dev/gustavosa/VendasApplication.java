32474752e7fa597918a4d6509a3ddf5fbe4c5903
gsbad <gsbadbr@gmail.com>
2024-03-30 22:47:26 -0300

    primeiro commit - inicialização do projeto maven + spring boot

---
+++ vendas/src/main/java/dev/gustavosa/VendasApplication.java
@@ -0,0 +1,9 @@
+package dev.gustavosa;
+
+import org.springframework.boot.SpringApplication;
+
+public class VendasApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(VendasApplication.class, args);
+    }
+}
