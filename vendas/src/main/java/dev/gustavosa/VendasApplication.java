e31714da83f431d853aa4e586bf2c0196cf922e2
gsbad <gsbadbr@gmail.com>
2024-04-01 23:17:50 -0300

    Testando @Configuration

--- vendas/src/main/java/dev/gustavosa/VendasApplication.java
+++ vendas/src/main/java/dev/gustavosa/VendasApplication.java
@@ -1,5 +1,7 @@
 package dev.gustavosa;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,12 +10,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class VendasApplication {
+
+    @Autowired
+    @Qualifier("applicationName")
+    private String applicationName;
+    @Autowired
+    @Qualifier("userName")
+    private String userName;
+
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
     }
 
     @GetMapping("/hello")
     public String olaMundo(){
-        return "Olá Mundo!";
+        return "Olá " + userName + "! Você está no " + applicationName;
     }
 }
