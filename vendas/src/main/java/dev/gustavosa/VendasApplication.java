f43f0faa9d3d9319be5e85e55bb41e7916f00157
gsbad <gsbadbr@gmail.com>
2024-04-02 21:46:54 -0300

    application.properties explorando

--- vendas/src/main/java/dev/gustavosa/VendasApplication.java
+++ vendas/src/main/java/dev/gustavosa/VendasApplication.java
@@ -1,7 +1,6 @@
 package dev.gustavosa;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,12 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class VendasApplication {
 
-    @Autowired
-    @Qualifier("applicationName")
+    @Value("${spring.application.name}")
     private String applicationName;
-    @Autowired
-    @Qualifier("userName")
-    private String userName;
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
@@ -24,6 +19,6 @@ public class VendasApplication {
 
     @GetMapping("/hello")
     public String olaMundo(){
-        return "Olá " + userName + "! Você está no " + applicationName;
+        return "Olá! Você está no " + applicationName;
     }
 }
